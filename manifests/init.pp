045587f20c16afecde618ba2f3740ad3cd288152
Ruslan Forostianov <ruslan@thehyve.nl>
2018-10-05 12:26:38 +0200

    Restart services when there is change to docker compose

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,8 +10,7 @@ class transmart_packer inherits transmart_packer::params {
     file { $docker_compose_file_path:
         ensure => file,
         content => template('transmart_packer/docker-compose.yml.erb'),
-    } 
-    -> docker_compose { 'packer_compose':
+    } ~> docker_compose { 'packer_compose':
         compose_files => [ $docker_compose_file_path ],
         ensure  => present,
     }
