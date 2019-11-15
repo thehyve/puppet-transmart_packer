e116abcdfcfe1caaae321c68034017e10b5cd50e
ewelinagr <ewelina@thehyve.nl>
2019-11-15 08:15:00 +0100

    Remove '$' from keycloak parameter name

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class transmart_packer::params (
     String $keycloak_server_url = lookup('transmart_packer::keycloak_server_url', String, first, ''),
     String $keycloak_realm = lookup('transmart_packer::keycloak_realm', String, first, 'dev'),
     String $keycloak_client_id = lookup('transmart_packer::keycloak_client_id', String, first, 'transmart'),
-    String $keycloak_offline_token = lookup('transmart_packer::$keycloak_offline_token', String, first, ''),
+    String $keycloak_offline_token = lookup('transmart_packer::keycloak_offline_token', String, first, ''),
     String $docker_compose_file_path = lookup('transmart_packer::docker_compose_file_path', String, first, 'tmp'),
     String $docker_image_version = lookup('transmart_packer::docker_image_version', String, first, 'latest'),
     String $client_origin_url = lookup('transmart_packer::client_origin_url', String, first, '*'),
@@ -18,7 +18,7 @@ class transmart_packer::params (
     }
 
     if $keycloak_offline_token == '' {
-        fail('No keycloak offline token specified. Please configure transmart_packer::$keycloak_offline_token')
+        fail('No keycloak offline token specified. Please configure transmart_packer::keycloak_offline_token')
     }
 
 }
